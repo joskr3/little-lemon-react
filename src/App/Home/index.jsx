96c90ebe2927198d91f3b8f17f130400f38d8df3
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2024-06-25 22:44:12 -0500

    se agrego el custom slider

--- src/App/Home/index.jsx
+++ src/App/Home/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import NavBar from '../../components/NavBar'
 import { Carousel } from 'nuka-carousel'
 import CustomSlider from '../../components/CustomSlider'
+import Button from '../../components/Button'
 
 const Home = () => {
 
@@ -44,6 +45,12 @@ const Home = () => {
     <>
       <NavBar />
       <CustomSlider sliders={data} />
+
+      <section className='flex gap-2 justify-around px-10  py-4 '>
+        <Button className='bg-green-700 text-white px-2 rounded-full' texto='Click' accion={() => console.log( "Hola mundo" )} />
+        <Button className='bg-green-700 text-white px-2 rounded-full' texto='Click' accion={() => console.log( "Hola mundo2" )} />
+        <Button className='bg-green-700 text-white px-2 rounded-full' texto='Click' accion={() => console.log( "Hola mundo3" )} />
+      </section>
     </>
   )
 }
