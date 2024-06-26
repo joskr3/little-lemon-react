8553c38fa7b637625b9a218365a668cc98e3e1df
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2024-06-25 20:30:05 -0500

    agregamos un ejemplo de uso de un carrousel de imagenes

--- src/App/Home/index.jsx
+++ src/App/Home/index.jsx
@@ -1,11 +1,19 @@
-import { Menu, ShoppingCart } from 'lucide-react'
 import React from 'react'
 import NavBar from '../../components/NavBar'
+import { Carousel } from 'nuka-carousel'
 
 const Home = () => {
   return (
     <>
-      <NavBar/>
+      <NavBar />
+      
+      <Carousel autoplay showArrows wrapMode="wrap">
+        <img src='/src/assets/dessetr1.jpg' width={600}  alt='postre 1' />
+        <img src='/src/assets/dessert2.jpg' width={1200}  alt='postre 2' />
+        <img src='/src/assets/dessetr1.jpg' width={1000} alt='postre 3' />
+        <img src='/src/assets/dessert2.jpg' width={1100} alt='postre 4' />
+        <img src='/src/assets/dessert3.jpg' width={700} alt='postre 5' />
+      </Carousel>
     </>
   )
 }
