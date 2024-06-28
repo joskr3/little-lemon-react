81808722a1f5c31eb4184ffdc67387119fcaa56c
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2024-06-27 20:41:44 -0500

    componente registro

--- src/App/Registro/index.jsx
+++ src/App/Registro/index.jsx
@@ -1,10 +1,55 @@
 import React from "react";
+import CustomNavBar from "../../components/CustomNavbar";
+import styles from "../styles";
+import { useForm } from 'react-hook-form';
 
 function Registro() {
+
+  const { estilosTitulo, formulario, contenedor, label, input, submit } = styles
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log( data );
+  console.log( errors );
+
   return (
-    <div>
-      Hola soy el registro
-    </div>
+
+    <>
+      <CustomNavBar />
+      <h1 className={estilosTitulo} >
+        Sign up
+      </h1>
+      <form onSubmit={handleSubmit( onSubmit )} className={formulario} >
+        <div className={contenedor}>
+          <label className={label} >First name</label>
+          <input type="text" placeholder="pedro pedro pedro" className={input} {...register( "firstname", { required: true, max: 32, min: 24 } )} />
+        </div>
+        <div className={contenedor}>
+          <label className={label} >Last name</label>
+          <input type="text" className={input} {...register( "lastname", { required: true, max: 32, min: 24, maxLength: 48 } )} />
+        </div>
+        <div className={contenedor}>
+          <label className={label} >Create a username</label>
+          <input type="text" placeholder="Kitten1234" className={input} {...register( "username", { required: true, max: 32, min: 5 } )} />
+        </div>
+        <div className={contenedor}>
+          <label className={label} >Phone number</label>
+          <input type="telf" placeholder="+519999999999" className={input} {...register( "phone", { required: true, max: 12, min: 12 } )} />
+        </div>
+        <div className={contenedor}>
+          <label className={label} >Email</label>
+          <input type="email" placeholder="mail@mail.coom" className={input} {...register( "email", { required: true, max: 32, min: 5 } )} />
+        </div>
+        <div className={contenedor}>
+          <label className={label} >Password</label>
+          <input type="password" className={input} {...register( "password", { required: true, max: 32, min: 8, maxLength: 48 } )} />
+        </div>
+        <div className={contenedor}>
+          <label className={label} >Confirm Password</label>
+          <input type="password" className={input} {...register( "repassword", { required: true, max: 32, min: 8, maxLength: 48 } )} />
+        </div>
+        <input type="submit" value="Submit" className={submit} />
+      </form>
+    </>
+
   )
 }
 
---
+++ src/App/styles.ts
@@ -0,0 +1,11 @@
+  const styles = {
+    estilosTitulo: "flex flex-col gap-1 text-green-700 italic m-auto text-pretty  mb-10 text-3xl md:text-8xl justify-center text-center",
+    formulario: "flex flex-col gap-3 mb-3",
+    contenedor: "flex  gap-4 text-white bg-green-700 p-3 max-w-fit rounded-2xl m-auto min-w-[360px] justify-between",
+    label: "text-sm  flex flex-col justify-center ",
+    input: "rounded-md p-1  text-green-700",
+    submit: "text-green-700 bg-yellow-300 rounded-full py-1 my-12 px-4 w-fit mb-3 m-auto text-sm min-w-[360px] min-h-[50px]",
+    hipervinculo: "flex no-underline text-yellow-500 m-auto justify-center"
+  }
+
+  export default styles
